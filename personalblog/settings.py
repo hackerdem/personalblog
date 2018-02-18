7c7b8188f38d3118f1c17127801b7638f320e1ac
Erdem Yilmaz <hackerdem@users.noreply.github.com>
2018-02-18 22:29:15 +1100

    frontend fixes and improvements

--- personalblog/settings.py
+++ personalblog/settings.py
@@ -37,7 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'blogpost.apps.BlogpostConfig'
+    'blogpost.apps.BlogpostConfig',
+    'taggit'
 ]
 
 MIDDLEWARE_CLASSES = [
